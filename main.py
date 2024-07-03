3cd7bed776b873df6cbbce6f1571437433bb8f64
TrevorBuchanan <123215971+TrevorBuchanan@users.noreply.github.com>
2024-07-03 15:50:16 -0700

    use r-squared and RMSE when measuring performance (r-squared not used, but function exists)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 
 from DataStructures.plot import Plot
-from Helpers.utility import percent_error, get_plot
+from Helpers.utility import get_plot, normalize_all_of_attr, calculate_rmse
 
 from Helpers.visualizer import Visualizer
 from Helpers.parser import Parser
@@ -10,6 +10,7 @@ from Helpers.parser import Parser
 from MachineLearningModule.LSTM.Univariate.uni_lstm_data_handler import UniLSTMDataHandler
 from MachineLearningModule.LSTM.Univariate.vanillaLSTM import VanillaLSTM
 from MachineLearningModule.LSTM.Univariate.stackedLSTM import StackedLSTM
+
 
 if __name__ == '__main__':
     print("AgAID Project\n")
@@ -21,16 +22,27 @@ if __name__ == '__main__':
 
     # Parsing selections
     season = "spring"
-    vi_formula = "ndvi"
 
     # ML model selections
     model_num = 1
-    target_variate = "vi_mean"
+    saved_data_set_num = 1
+    target_variate = "ndvi"
 
     # Perform parsing based on selections
     parser = Parser()
-    parser.parse_data(season, plots, vi_formula)
+    parser.parse_data(season, plots)
     data_handler = UniLSTMDataHandler(plots)
+
+    # Perform normalizations
+    normalize_all_of_attr(plots, "cigreen0")
+    normalize_all_of_attr(plots, "cigreen")
+    normalize_all_of_attr(plots, "evi2")
+    normalize_all_of_attr(plots, "gndvi0")
+    normalize_all_of_attr(plots, "gndvi")
+    normalize_all_of_attr(plots, "ndvi")
+    normalize_all_of_attr(plots, "rdvi")
+    normalize_all_of_attr(plots, "savi")
+    normalize_all_of_attr(plots, "sr")
 
     # Data preparation for machine learning
     # data_handler.make_sets(target_variate, 80)
@@ -51,36 +63,52 @@ if __name__ == '__main__':
     # Create visualizer
     visualizer = Visualizer()
     visualizer.line_mode = True
-    visualizer.show_vi_mean = True
+    visualizer.show_cigreen0 = True
+    visualizer.show_cigreen = True
+    visualizer.show_evi2 = True
+    visualizer.show_gndvi0 = True
+    visualizer.show_gndvi = True
+    visualizer.show_ndvi = True
+    visualizer.show_rdvi = True
+    visualizer.show_savi = True
+    visualizer.show_sr = True
     visualizer.show_heading_date = True
-    visualizer.show_yield = True
-    visualizer.show_prediction = True
+    # visualizer.show_yield = True
+    # visualizer.show_prediction = True
 
     # Test the model and show results
     data_handler.make_predictions_and_accuracies_for_test_sets(learning_model)
-    total_accuracies = []
+    total_percent_errors = []
+    total_rmse_errors = []
     for prediction_tup, accuracies_tup in zip(data_handler.predictions, data_handler.accuracies):
         entry_bloc_pairs = [(prediction_tup[1], prediction_tup[2])]
-        acc = sum(accuracies_tup[0]) / len(accuracies_tup[0])
-        total_accuracies.append(acc)
-        print(f'Average percent error (testing data): {acc}')
-        visualizer.visualize_plots(plots, entry_bloc_pairs, prediction_tup[0])
+        percent_e_acc = sum(accuracies_tup[0]) / len(accuracies_tup[0])
+        total_percent_errors.append(percent_e_acc)
+        print(f'Average percent error (testing data): {percent_e_acc}')
+        goal_val = get_plot(prediction_tup[1], prediction_tup[2], plots).crop_yield
+        rmse_error = calculate_rmse(goal_val, prediction_tup[0])
+        total_rmse_errors.append(rmse_error)
+        print(f'RMSE error (testing data): {rmse_error}')
+        # visualizer.visualize_plots(plots, entry_bloc_pairs, prediction_tup[0])
         print()
-    print(f'Model average percent error (testing data): {sum(total_accuracies) / len(total_accuracies)}')
+    print(f'Model average percent error (testing data): {sum(total_percent_errors) / len(total_percent_errors)}')
+    print(f'Model average RMSE (testing data): {sum(total_rmse_errors) / len(total_rmse_errors)}')
 
     # data_handler.continue_training_on_weak_sets(learning_model, 2)
 
-    # Check model's performance on training data and show results
-    data_handler.make_predictions_and_accuracies_for_training_sets(learning_model)
-    total_accuracies = []
-    for prediction_tup, accuracies_tup in zip(data_handler.predictions, data_handler.accuracies):
-        entry_bloc_pairs = [(prediction_tup[1], prediction_tup[2])]
-        acc = sum(accuracies_tup[0]) / len(accuracies_tup[0])
-        total_accuracies.append(acc)
-        print(f'Average percent error (training data): {acc}')
-        # visualizer.visualize_plots(plots, entry_bloc_pairs, prediction_tup[0])
-        print()
-    print(f'Model average percent error (training data): {sum(total_accuracies) / len(total_accuracies)}')
+    # # Check model's performance on training data and show results
+    # data_handler.make_predictions_and_accuracies_for_training_sets(learning_model)
+    # total_percent_errors = []
+    # total_r_squared_errors = []
+    # total_rmse_errors = []
+    # for prediction_tup, accuracies_tup in zip(data_handler.predictions, data_handler.accuracies):
+    #     entry_bloc_pairs = [(prediction_tup[1], prediction_tup[2])]
+    #     percent_e_acc = sum(accuracies_tup[0]) / len(accuracies_tup[0])
+    #     total_percent_errors.append(percent_e_acc)
+    #     print(f'Average percent error (training data): {percent_e_acc}')
+    #     visualizer.visualize_plots(plots, entry_bloc_pairs, prediction_tup[0])
+    #     print()
+    # print(f'Model average percent error (training data): {sum(total_percent_errors) / len(total_percent_errors)}')
 
     # Visualize plot
     # visualizer.visualize_plots(plots, [(1, 1)])
